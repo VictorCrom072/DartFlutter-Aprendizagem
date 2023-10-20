7e912362a6b7758ddeb9f8cdb85ae7b488ab2b10
VictorCrom072 <portovictor072@gmail.com>
2023-10-19 23:38:11 -0300

    DartBackEnd-Aula04

--- dart_backend/dart_projeto1/bin/dart_projeto1.dart
+++ dart_backend/dart_projeto1/bin/dart_projeto1.dart
@@ -1,9 +1,11 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'server_handler.dart';
 
 void main() async {
+  var _server = Serverhandler();
   final servidor = await shelf_io.serve(
-    (request) => Response(200, body: 'ok'), 'localhost', 8080
+    _server.handler, 'localhost', 8080
   );
   print('Nosso server foi iniciado => http://localhost:8080');
 }
