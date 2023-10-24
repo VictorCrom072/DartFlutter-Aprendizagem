a82df18d8b0a3df7b114f95ba43b410037bd82ee
VictorCrom072 <portovictor072@gmail.com>
2023-10-23 22:10:51 -0300

    DartBackEnd-aula07

--- dart_backend/dart_projeto1/bin/server_handler.dart
+++ dart_backend/dart_projeto1/bin/server_handler.dart
@@ -6,25 +6,36 @@ import 'package:shelf_router/shelf_router.dart';
 class Serverhandler {
   Handler get handler {
     final router = Router();
-    router.get('/', (Request request){
-      return Response(200, body: 'Primeira rota');
+    router.get('/', (Request request) {
+      return Response(200,
+          body: '<h1>Primeira rota<h1>',
+          headers: {'content-type': 'text/html'});
     });
-    router.get('/ola/mundo', (Request request, String usuario){
-      return Response.ok(headers: {'Content-Type': 'text/plain'}, 'Olá Mundo $usuario!!');
+    router.get('/ola/mundo', (Request request, String usuario) {
+      return Response.ok(
+          headers: {'Content-Type': 'text/plain'}, 'Olá Mundo $usuario!!');
     });
     //HTTP GET
     //http://localhost:8080/query?nome=Crom&idade=18
-    router.get('/query', (Request request){
+    router.get('/query', (Request request) {
       String? nome = request.url.queryParameters['nome'];
       String? idade = request.url.queryParameters['idade'];
-      return Response.ok(headers: {'Content-Type': 'text/plain'}, 'Query eh: $nome, $idade');
+      return Response.ok(
+          headers: {'Content-Type': 'text/plain'}, 'Query eh: $nome, $idade');
     });
-    router.post('/login', (Request request) async{
+    router.post('/login', (Request request) async {
       var result = await request.readAsString();
-      Map json =  jsonDecode(result);
-      if(json['tipo'] == 'adm' && json['senha'] == 'uDroga')
-        return Response.ok('Admininastrô');
-      else
+      Map json = jsonDecode(result);
+      if (json['tipo'] == 'adm' && json['senha'] == 'uDroga') {
+        Map result = {'token': '12345678', 'id_user': 1};
+        var jsonResponse = jsonEncode(result);
+        return Response.ok(
+          jsonResponse,
+          headers: {
+            'content-type' : 'application/json'
+          }
+        );
+      } else
         return Response.ok('loko e soinhádô');
     });
     return router;
