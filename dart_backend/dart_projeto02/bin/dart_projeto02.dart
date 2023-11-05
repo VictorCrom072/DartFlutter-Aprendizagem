126eaa361a0daf24c0c6e371fabf72abcfa9a926
VictorCrom072 <portovictor072@gmail.com>
2023-11-04 23:52:11 -0300

    DartBackEnd-aula31

--- dart_backend/dart_projeto02/bin/dart_projeto02.dart
+++ dart_backend/dart_projeto02/bin/dart_projeto02.dart
@@ -1,10 +1,10 @@
-
 import 'package:shelf/shelf.dart';
 
 import 'api/noticia_api.dart';
 import 'api/login_api.dart';
 import 'infra/custom_server.dart';
 import 'infra/dart_injector/injects.dart';
+import 'infra/database/db_configuration.dart';
 import 'infra/middleware.interception.dart';
 import 'utils/custom_env.dart';
 
@@ -12,6 +12,10 @@ void main() async {
   CustomEnv.fromFile('.env');
 
   final _di = Injects.initialize();
+
+  var conn = await _di.get<DBConfiguration>().connection;
+  var results = await conn.query("SELECT * FROM usuarios;");
+  print(results);
 
   var cascadeHandler = Cascade()
     .add(_di.get<LoginApi>().getHandler())
--- dart_backend/dart_projeto02/bin/infra/dart_injector/injects.dart
+++ dart_backend/dart_projeto02/bin/infra/dart_injector/injects.dart
@@ -3,6 +3,8 @@ import '../../api/noticia_api.dart';
 import '../../models/noticia_model.dart';
 import '../../services/generic_service.dart';
 import '../../services/noticia_service.dart';
+import '../database/db_configuration.dart';
+import '../database/mysql_db_configuration.dart';
 import '../security/security_service.dart';
 import '../security/security_service_imp.dart';
 import 'dependency_injector.dart';
@@ -10,6 +12,10 @@ import 'dependency_injector.dart';
 class Injects {
   static DependencyInjector initialize(){
     var di = DependencyInjector();
+
+    di.register<DBConfiguration>(
+      () => MySqlDBConfiguration(),
+    );
 
     di.register<SecurityService>(
       () => SecurityServiceImp(),
